94aab2778767255329ff6940ff237c1bb04b0b5f
Denis <wygndo@gmail.com>
2026-08-27 12:04:28 +0300

    1.0.0

--- apps/api-gateway/src/modules/users/controllers/controller.ts
+++ apps/api-gateway/src/modules/users/controllers/controller.ts
@@ -1,8 +1,11 @@
-import { UserConnectRequestDTO } from '@modules/users/dtos';
+import {
+  UserConnectRequestDTO,
+  UserConnectResponseDTO,
+} from '@modules/users/dtos';
 import { UserMapper } from '@modules/users/mappers';
 import { UserService } from '@modules/users/services/service';
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Пользователи')
 @Controller({
@@ -13,6 +16,10 @@ export class UserControllerV1 {
   constructor(private readonly userService: UserService) {}
 
   @ApiOperation({ summary: 'Подключение пользователя' })
+  @ApiOkResponse({
+    type: UserConnectResponseDTO,
+    description: 'Успешный ответ',
+  })
   @Post('connect')
   public async connectUser(@Body() body: UserConnectRequestDTO) {
     return this.userService.connectUser(UserMapper.toDomainModel(body));
---
+++ apps/api-gateway/src/modules/users/dtos/connect/response.ts
@@ -0,0 +1,19 @@
+import { PlatformEnum } from '@addy/common';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UserConnectResponseDTO {
+  @ApiProperty({
+    type: String,
+    description: 'Одноразовый код для подключения',
+    required: true,
+    example: 'XXX-YYY',
+  })
+  code: string;
+
+  @ApiProperty({
+    type: String,
+    description: `Ссылка для подключения. Актуальна для platform=${PlatformEnum.TELEGRAM}`,
+    required: false,
+  })
+  connectionLink: string | undefined;
+}
